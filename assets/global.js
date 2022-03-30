4861ccb127eb57de246fb4b5daf7dccf13f7888f
Dustin Graham <dustin@extend.com>
2022-03-30 11:14:56 -0600

    cleanup global.js

--- assets/global.js
+++ assets/global.js
@@ -1,50 +1,54 @@
-let store_id = window.ExtendWooCommerce.store_id;
-let environment = window.ExtendWooCommerce.environment;
-let ajaxurl = window.ExtendWooCommerce.ajaxurl;
-
 jQuery(document).ready(function() {
+	if(!ExtendWooCommerce) return;
+
+	const { store_id: storeId, environment } = ExtendWooCommerce;
+
     Extend.config({
-		storeId: store_id,
-		environment: environment
+		storeId,
+		environment
 	});
 
 	window.ExtendWooCommerce = {
-		...window.ExtendWooCommerce,
+		...ExtendWooCommerce,
 		addPlanToCart,
 		getCart,
 		warrantyAlreadyInCart
 	}
 })
 
-async function addPlanToCart (opts) {
-	return await jQuery.post(ajaxurl, {
-		action: "add_to_cart_extend",
-		quantity: opts.quantity,
-		extendData: opts.plan
-	}).promise()
-}
+if(ExtendWooCommerce) {
+	const { ajaxurl } = ExtendWooCommerce;
 
-async function getCart() {
-	return JSON.parse(
-		await jQuery.post(ajaxurl, {
-			action: "get_cart_extend"
+	async function addPlanToCart (opts) {
+		return await jQuery.post(ajaxurl, {
+			action: "add_to_cart_extend",
+			quantity: opts.quantity,
+			extendData: opts.plan
 		}).promise()
-	);
-}
-
-function warrantyAlreadyInCart (variantId, cart) {
-    var cartItems = Object.values(cart['cart_contents']);
-    const extendWarranties = cartItems.filter(function (lineItem) {
-      //filter through the customAttributes and grab the referenceId
-      var extendData = lineItem.extendData;
-      if (extendData && extendData['covered_product_id'])
-        var referenceId = extendData['covered_product_id'];
-      return (
-        extendData &&
-        !extendData.leadToken &&
-        referenceId &&
-        referenceId.toString() === variantId.toString()
-      );
-    });
-    return extendWarranties.length > 0;
+	}
+	
+	async function getCart() {
+		return JSON.parse(
+			await jQuery.post(ajaxurl, {
+				action: "get_cart_extend"
+			}).promise()
+		);
+	}
+	
+	function warrantyAlreadyInCart (variantId, cart) {
+		var cartItems = Object.values(cart['cart_contents']);
+		const extendWarranties = cartItems.filter(function (lineItem) {
+		  //filter through the customAttributes and grab the referenceId
+		  var extendData = lineItem.extendData;
+		  if (extendData && extendData['covered_product_id'])
+			var referenceId = extendData['covered_product_id'];
+		  return (
+			extendData &&
+			!extendData.leadToken &&
+			referenceId &&
+			referenceId.toString() === variantId.toString()
+		  );
+		});
+		return extendWarranties.length > 0;
+	}
 }
