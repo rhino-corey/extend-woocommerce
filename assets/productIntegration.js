2a1263c3d5fd68c5bc83170a19aeadb633c26411
Santiago Enciso <santiago.enciso@extend.com>
2022-04-06 12:03:58 -0500

    Adds atcButton variable with correct selector and updates all references of the old selector with atcButton

--- assets/productIntegration.js
+++ assets/productIntegration.js
@@ -1,7 +1,9 @@
 jQuery(document).ready(function(){
-	if(!ExtendWooCommerce || !ExtendProductIntegration) return;
+    if(!ExtendWooCommerce || !ExtendProductIntegration) return;
 
-	const { type: product_type, id: product_id, extend_modal_offers_enabled, extend_pdp_offers_enabled } = ExtendProductIntegration;
+    const { type: product_type, id: product_id, extend_modal_offers_enabled, extend_pdp_offers_enabled } = ExtendProductIntegration;
+
+    let atcButton = 'button.addToCart';
 
     if(extend_pdp_offers_enabled === 'no'){
         var extendOffer = document.querySelector('#extend-offer')
@@ -22,29 +24,29 @@ jQuery(document).ready(function(){
             let variation_id = jQuery('[name="variation_id"]').val();
             if(variation_id ) {
                 let comp = Extend.buttons.instance('#extend-offer');
-                    comp.setActiveProduct(variation_id)
+                comp.setActiveProduct(variation_id)
             }
-		}, 500);
+        }, 500);
 
-		jQuery( ".single_variation_wrap" ).on( "show_variation", function ( event, variation )  {
-			let component = Extend.buttons.instance('#extend-offer');
-			variation_id = variation.variation_id;
+        jQuery( ".single_variation_wrap" ).on( "show_variation", function ( event, variation )  {
+            let component = Extend.buttons.instance('#extend-offer');
+            variation_id = variation.variation_id;
 
-			if(variation_id) {
-				component.setActiveProduct(variation.variation_id)
-			}
-		});
+            if(variation_id) {
+                component.setActiveProduct(variation.variation_id)
+            }
+        });
 
     }
 
-    jQuery('button.single_add_to_cart_button').on('click', function extendHandler(e) {
-		e.preventDefault()
+    jQuery(atcButton).on('click', function extendHandler(e) {
+        e.preventDefault()
 
-		function triggerAddToCart() {
-			jQuery('button.single_add_to_cart_button').off('click', extendHandler);
-			jQuery('button.single_add_to_cart_button').trigger('click');
-			jQuery('button.single_add_to_cart_button').on('click', extendHandler);
-		}
+        function triggerAddToCart() {
+            jQuery(atcButton).off('click', extendHandler);
+            jQuery(atcButton).trigger('click');
+            jQuery(atcButton).on('click', extendHandler);
+        }
 
         // /** get the component instance rendered previously */
         const component = Extend.buttons.instance('#extend-offer');
@@ -54,30 +56,30 @@ jQuery(document).ready(function(){
         const product = component.getActiveProduct();
 
         if (plan) {
-			var planCopy = { ...plan, covered_product_id: product.id }
-			var data = {
-				quantity: 1,
-				plan: planCopy
-			}
-			ExtendWooCommerce.addPlanToCart(data)
-				.then(() => {
-					triggerAddToCart()
-				})
+            var planCopy = { ...plan, covered_product_id: product.id }
+            var data = {
+                quantity: 1,
+                plan: planCopy
+            }
+            ExtendWooCommerce.addPlanToCart(data)
+              .then(() => {
+                  triggerAddToCart()
+              })
         } else{
             if(extend_modal_offers_enabled === 'yes'){
                 Extend.modal.open({
                     referenceId: product.id,
                     onClose: function(plan, product) {
                         if (plan && product) {
-							var planCopy = { ...plan, covered_product_id: product.id }
-							var data = {
-								quantity: 1,
-								plan: planCopy
-							}
-							ExtendWooCommerce.addPlanToCart(data)
-								.then(() => {
-									triggerAddToCart()
-								})
+                            var planCopy = { ...plan, covered_product_id: product.id }
+                            var data = {
+                                quantity: 1,
+                                plan: planCopy
+                            }
+                            ExtendWooCommerce.addPlanToCart(data)
+                              .then(() => {
+                                  triggerAddToCart()
+                              })
                         } else {
                             triggerAddToCart()
                         }
